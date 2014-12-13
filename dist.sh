f4c2eccf58cb44c9804633d447a4d14670549eda
frankdelange <github-f@unternet.org>
2014-12-13 14:28:37 +0100

    Added dist.sh script to create dist files

---
+++ dist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# creates dist files for all updated apps
+
+ls */appinfo/info.xml|while read f;do
+	app=$(echo $f|cut -d '/' -f 1)
+	version=$(xml2json < $f|jq -r '.info.version["$t"]')
+	if [ ! -f dist/${app}-${version}.tar.gz ]; then
+		find ${app} -print0 -type f|tar cvzf dist/${app}-${version}.tar.gz --null -T -
+	else
+		echo ${app} version ${version} up to date
+	fi
+done
