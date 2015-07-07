36ed86bbfbf18eb3c982307bbcd5fd1bac494867
frankdelange <github-f@unternet.org>
2015-07-07 23:04:51 +0200

    files_opds: updated dist file, it contained duplicate files...

--- dist.sh
+++ dist.sh
@@ -6,7 +6,7 @@ ls */appinfo/info.xml|while read f;do
 	app=$(echo $f|cut -d '/' -f 1)
 	version=$(xml2json < $f|jq -r '.info.version["$t"]')
 	if [ ! -f dist/${app}-${version}.tar.gz ]; then
-		find ${app} -print0 -type f|tar cvzf dist/${app}-${version}.tar.gz --null -T -
+		find ${app} -type f -print0|tar cvzf dist/${app}-${version}.tar.gz --null -T -
 	else
 		echo ${app} version ${version} up to date
 	fi
