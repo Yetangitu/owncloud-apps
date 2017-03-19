03355c53b5b0a47695c045af992d46d57fdb7429
frankdelange <github-f@unternet.org>
2017-03-19 22:07:19 +0100

    files_reader: prepare for NC12

--- files_reader/js/plugin.js
+++ files_reader/js/plugin.js
@@ -46,6 +46,7 @@
 
 		hideControls: function() {
 			$('#app-content #controls').hide();
+            $('#app-navigation').css("display", "none");
 		},
 
 		hide: function() {
@@ -54,6 +55,7 @@
 			}
 			$("#controls").show();
 			$('#app-content #controls').removeClass('hidden');
+            $('#app-navigation').css("display", "");
 			if ($('#isPublic').val()) {
 				$('#imgframe').show();
 				$('footer').show();
