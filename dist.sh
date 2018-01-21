c25ad42ad152c6fa48dd61682d65422f7096927e
frankdelange <github-f@unternet.org>
2018-01-21 22:52:11 +0100

    - modified dist.sh to create separate dist files for OC and NC due to signature incompatibilities between the two.  - added dist/*.sig to .gitignore

--- dist.sh
+++ dist.sh
@@ -5,8 +5,11 @@
 ls */appinfo/info.xml|while read f;do
 	app=$(echo $f|cut -d '/' -f 1)
 	version=$(xml2json < $f|jq -r '.info.version["$t"]')
-	if [ ! -f dist/${app}-${version}.tar.gz ]; then
-		find ${app} -type f -print0|tar cvzf dist/${app}-${version}.tar.gz --null -T -
+	if [ ! -f dist/${app}-${version}-OC.tar.gz ]; then
+		find ${app} -type f -print0|tar --exclude=${app}/appinfo/signature.json -cvzf dist/${app}-${version}-NC.tar.gz --null -T -
+        nextcloud_sign_package ${app} ${version} > dist/${app}-${version}-NC.sig
+        owncloud_sign_package ${app}
+		find ${app} -type f -print0|tar cvzf dist/${app}-${version}-OC.tar.gz --null -T -
 	else
 		echo ${app} version ${version} up to date
 	fi
